ab66d79a50b01aa27bda26fbd92ac9ca817a2f41
Arun Magar <70889447+arunmagar-github@users.noreply.github.com>
2022-06-24 15:28:08 +0530

    modified code

--- json_snowflake_app.py
+++ json_snowflake_app.py
@@ -11,7 +11,7 @@ streamlit.title("Order Delivery status")
 #snowflake related functions
 def get_running_order_list():
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("select order_details:order_id  ,order_details:user_id,order_details:user_name ,order_details:order_datetime,order_details:delivery_datetime ,order_details:status,order_details:email,order_details:origin_location ,order_details:destination_location from order_db.public.orders where order_details:status !='delivered'")
+    my_cur.execute("select order_details:order_id,order_details:user_id,order_details:user_name ,order_details:order_datetime ,order_details:status,order_details:email,order_details:origin_location ,order_details:destination_location from order_db.public.orders where order_details:status !='delivered'")
     return my_cur.fetchall()
 
 def get_completed_order_list():
