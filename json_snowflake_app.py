e339e4631b26cb898d4479152ac47ddcc8069f7a
Arun Magar <70889447+arunmagar-github@users.noreply.github.com>
2022-06-24 12:33:39 +0530

    fixed query

--- json_snowflake_app.py
+++ json_snowflake_app.py
@@ -10,16 +10,16 @@ streamlit.title("Order Delivery status")
 #snowflake related functions
 def get_order_list():
   with my_cnx.cursor() as my_cur:
-    my_cur.execute('select order_details:"order_id" as "orderId" ,
-order_details:"user_id" as "userId",
-order_details:"user_name" as "UserName",
-order_details:"order_datetime" as "OrderDateTime",
-order_details:"delivery_datetime" as "DeliveryDatetime",
-order_details:"status" as "CurrentStatus",
-order_details:"email" as "UserEmail",
-order_details:"origin_location" as "Origin",
-order_details:"destination_location" as "Destination"
-from order_db.public.orders')
+    my_cur.execute("select order_details:'order_id' as 'orderId' ,
+                    order_details:'user_id' as 'userId',
+                    order_details:'user_name' as 'UserName',
+                    order_details:'order_datetime' as 'OrderDateTime',
+                    order_details:'delivery_datetime' as 'DeliveryDatetime',
+                    order_details:'status' as 'CurrentStatus',
+                    order_details:'email' as 'UserEmail',
+                    order_details:'origin_location' as 'Origin',
+                    order_details:'destination_location' as 'Destination'
+                    from order_db.public.orders")
     return my_cur.fetchall()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
