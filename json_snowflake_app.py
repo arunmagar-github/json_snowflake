15435133eb4d06d5d271e59dd40d75f9c0a3017c
Arun Magar <70889447+arunmagar-github@users.noreply.github.com>
2022-06-24 15:37:50 +0530

    remove double quotes from output

--- json_snowflake_app.py
+++ json_snowflake_app.py
@@ -11,12 +11,12 @@ streamlit.title("Order Delivery status")
 #snowflake related functions
 def get_running_order_list():
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("select order_details:order_id,order_details:user_id,order_details:user_name ,order_details:order_datetime ,order_details:status,order_details:email,order_details:origin_location ,order_details:destination_location from order_db.public.orders where order_details:status !='delivered'")
+    my_cur.execute("select order_details:order_id::varchar,order_details:user_id::varchar,order_details:user_name::varchar ,order_details:order_datetime::varchar,order_details:status::varchar,order_details:email::varchar,order_details:origin_location::varchar,order_details:destination_location::varchar from order_db.public.orders where order_details:status !='delivered'")
     return my_cur.fetchall()
 
 def get_completed_order_list():
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("select order_details:order_id  ,order_details:user_id,order_details:user_name ,order_details:order_datetime,order_details:delivery_datetime ,order_details:status,order_details:email,order_details:origin_location ,order_details:destination_location from order_db.public.orders where order_details:status ='delivered'")
+    my_cur.execute("select order_details:order_id::varchar,order_details:user_id::varchar,order_details:user_name::varchar,order_details:order_datetime::varchar,order_details:delivery_datetime::varchar,order_details:status::varchar,order_details:email::varchar,order_details:origin_location::varchar,order_details:destination_location::varchar from order_db.public.orders where order_details:status ='delivered'")
     return my_cur.fetchall()  
   
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
