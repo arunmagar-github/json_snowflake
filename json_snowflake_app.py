6f1217a129bb54553be0bbe94dc9ac6411d5e5d6
Arun Magar <70889447+arunmagar-github@users.noreply.github.com>
2022-06-24 12:22:19 +0530

    snowflake connection

--- json_snowflake_app.py
+++ json_snowflake_app.py
@@ -6,3 +6,14 @@ from urllib.error import URLError
 
 streamlit.title("Order Delivery status")
 streamlit.header("Order not delivered yet")
+
+#snowflake related functions
+def get_order_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("SELECT * FROM ORDER_DB.PUBLIC.ORDERS")
+    return my_cur.fetchall()
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_data_rows=get_order_list()
+my_cnx.close();
+streamlit.dataframe(my_data_rows)
