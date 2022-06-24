3f8f700021dcc81074fe27ece4e44a009a4e9060
Arun Magar <70889447+arunmagar-github@users.noreply.github.com>
2022-06-24 15:39:15 +0530

    query sort

--- json_snowflake_app.py
+++ json_snowflake_app.py
@@ -11,12 +11,12 @@ streamlit.title("Order Delivery status")
 #snowflake related functions
 def get_running_order_list():
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("select order_details:order_id::varchar,order_details:user_id::varchar,order_details:user_name::varchar ,order_details:order_datetime::varchar,order_details:status::varchar,order_details:email::varchar,order_details:origin_location::varchar,order_details:destination_location::varchar from order_db.public.orders where order_details:status !='delivered'")
+    my_cur.execute("select order_details:order_id::varchar,order_details:user_id::varchar,order_details:user_name::varchar ,order_details:order_datetime::varchar,order_details:status::varchar,order_details:email::varchar,order_details:origin_location::varchar,order_details:destination_location::varchar from order_db.public.orders where order_details:status !='delivered' order by order_details:order_id")
     return my_cur.fetchall()
 
 def get_completed_order_list():
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("select order_details:order_id::varchar,order_details:user_id::varchar,order_details:user_name::varchar,order_details:order_datetime::varchar,order_details:delivery_datetime::varchar,order_details:status::varchar,order_details:email::varchar,order_details:origin_location::varchar,order_details:destination_location::varchar from order_db.public.orders where order_details:status ='delivered'")
+    my_cur.execute("select order_details:order_id::varchar,order_details:user_id::varchar,order_details:user_name::varchar,order_details:order_datetime::varchar,order_details:delivery_datetime::varchar,order_details:status::varchar,order_details:email::varchar,order_details:origin_location::varchar,order_details:destination_location::varchar from order_db.public.orders where order_details:status ='delivered'  order by order_details:order_id")
     return my_cur.fetchall()  
   
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
